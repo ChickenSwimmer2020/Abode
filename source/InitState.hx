8fe27854228c7e1a221d16b0768b5cfa65b57e18
ChickenSwimmer2020 <105177647+ChickenSwimmer2020@users.noreply.github.com>
2026-03-28 10:06:51 -0400

    shits fucked. i might go back to flixel if vector works there.

--- source/InitState.hx
+++ source/InitState.hx
@@ -2,7 +2,6 @@ package;
 
 import backend.utils.ATimer;
 import openfl.display.BitmapData;
-import rendering.BlurredSprite;
 import openfl.filters.BlurFilter;
 import openfl.filters.ShaderFilter;
 import lime.graphics.Image;
@@ -14,16 +13,21 @@ class InitState extends State {
     public function new() {
         super();
 
-        //TODO: make dynamic wallpaper loading work properly without crashing the program.
-        wallpaperBackground = new ASprite(0, 0).loadGraphic('assets/images/FbutteRAUtah.png'/*ASprite.getDesktopWallpaper(1280, 720)*/); //TODO: blur shader
+        var wallpaperBG:BitmapData = BitmapData.fromFile('assets/images/FbutteRAUtah.png'); //TODO: make user desktop image loading work.
+        wallpaperBackground = new ASprite(0, 0).loadGraphic(wallpaperBG/*ASprite.getDesktopWallpaper(1280, 720)*/);
         add(wallpaperBackground);
 
+
+        var dest = new openfl.geom.Point(50, 50); // where to write the result
+
+        wallpaperBG.applyFilter(wallpaperBG, wallpaperBG.rect, dest, new BlurFilter(8, 8, 2));
+
         ATimer.start(0.25, ()->{
-            var testBlurSprite:BlurredSprite = new BlurredSprite(1280, 720, 0x000000, 0, BitmapData.fromFile('assets/images/FbutteRAUtah.png'), 4, 2);
-            add(testBlurSprite);
-            var secondaryTestBlurSprite:BlurredSprite = new BlurredSprite(500, Main.pHeight, 0x000000, 0.25, BitmapData.fromFile('assets/images/FbutteRAUtah.png'), 16, 2);
-            secondaryTestBlurSprite.x = Main.pWidth-secondaryTestBlurSprite.width;
-            add(secondaryTestBlurSprite);
+            //var testBlurSprite:BlurredSprite = new BlurredSprite(1280, 720, 0x000000, 0, BitmapData.fromFile('assets/images/FbutteRAUtah.png'), 4, 2);
+            //add(testBlurSprite);
+            //var secondaryTestBlurSprite:BlurredSprite = new BlurredSprite(500, Main.pHeight, 0x000000, 0.25, BitmapData.fromFile('assets/images/FbutteRAUtah.png'), 16, 2);
+            //secondaryTestBlurSprite.x = Main.pWidth-secondaryTestBlurSprite.width;
+            //add(secondaryTestBlurSprite);
         });
     }
 }
